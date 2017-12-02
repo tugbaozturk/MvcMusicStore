c6653e4092f9921a2e859eee425e8875eb1afba4
Tuğba Öztürk <ttugbaozturk92@gmail.com>
2017-12-03 02:07:08 +0300

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using MvcMusicStore.Models;
+using MVCMusicStore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace MvcMusicStore
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            MusicStoreEntities db = new MusicStoreEntities();
+            var genres = db.Genres.ToList();
+            #region Simple MemberShip Adimlari
+            #endregion
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
